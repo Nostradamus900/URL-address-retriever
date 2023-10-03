4ab0075caa52b2eae03f3f239804f858ad73af01
Nostradamus900 <75764627+Nostradamus900@users.noreply.github.com>
2023-10-03 12:48:25 -0400

    Update Url_address_retriever.py

--- Url_address_retriever.py
+++ Url_address_retriever.py
@@ -4,7 +4,6 @@ file_path = input('Please enter html file path here: ')
 with open(file_path) as html:
     content = html.read()
     url_matches = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', content)
-    cleaned_list = list(dict.fromkeys(url_matches))
-    url_matches = ','.join(url_matches)
+    cleaned_url = list(dict.fromkeys(url_matches))
     
-print('\n'.join(cleaned_list))
+print('\nFound URL........................\n\n','\n '.join(cleaned_url))
