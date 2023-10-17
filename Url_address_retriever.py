87bd83adcfdeaa6fb689b006ec9e384251a2b628
Nostradamus900 <75764627+Nostradamus900@users.noreply.github.com>
2023-10-17 19:24:09 -0400

    Update Url_address_retriever.py

--- Url_address_retriever.py
+++ Url_address_retriever.py
@@ -1,10 +1,17 @@
 import re
+while True:
 
-file_path = input('Please enter html file path here: ')
-with open(file_path) as html:
-    content = html.read()
-    url_matches = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', content)
-    cleaned_url = list(dict.fromkeys(url_matches))
-    
-print('\nFound URL........................\n\n','\n '.join(cleaned_url))
-print('\nEOF..............................')
+    try:
+        file_path = str(input('Please enter file path here: '))
+        with open(file_path) as html:
+            content = html.read()
+            url_matches = re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', content)
+            cleaned_url = list(dict.fromkeys(url_matches))
+
+    except FileNotFoundError as err:
+        print('File does not exist')
+
+    else:
+        print('\nFound URL...................\n\n','\n '.join(cleaned_url))
+        print('\nEOF.........................')
+        break
